25c2d53dadcf7bd1368b5be2fa4b24b9b52051e5
Vlad Grigorescu <grigorescu@gmail.com>
2012-06-24 13:42:11 -0400

    Minor changes - directory reorganizing, and code cleanup.

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ setup(
     version = "0.1",
     packages = find_packages(),
     install_requires = ["Django", "requests"],
-    package_data = {'': ['*.js', '*.css', '*.png', '*.html']},
+    package_data = {'view':
+                        ['static/css/*.css',
+                         'static/img/*.png',
+                         'static/js/*.js',
+                         'templates/*.html',
+                         'templates/include/*.html',
+                        ]},
     include_package_data = True,
     zip_safe = False,
 )
