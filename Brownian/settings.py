5935139ed51373f9e1074b549d8575fb1b754a5a
Vlad Grigorescu <grigorescu@gmail.com>
2012-06-28 12:15:44 -0400

    Fix logging issue, and bump version number.

--- Brownian/settings.py
+++ Brownian/settings.py
@@ -116,6 +116,14 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
