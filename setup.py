cbc150432f790f80c03e225f67694f523246b51d
Vlad Grigorescu <grigorescu@gmail.com>
2012-07-12 16:55:20 -0400

    Show animated GIF.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 setup(
     name = "Brownian",
-    version = "0.3-r46",
+    version = "0.3-r47",
     packages = ["Brownian", "Brownian.view", "Brownian.view.templatetags", "Brownian.view.utils"],
     install_requires = ["Django", "django-dajax", "django-dajaxice", "requests", "nose", "pytz"],
     package_data = {'Brownian.view':
                         ['static/css/*.css',
                          'static/img/*.png',
+                         'static/img/*.gif',
                          'static/js/*.js',
                          'templates/*.html',
                          'templates/include/*.html',
