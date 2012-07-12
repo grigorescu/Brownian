94549030f2166c84ffc6a6d084aaaf1715bae3b3
Vlad Grigorescu <grigorescu@gmail.com>
2012-07-11 22:57:08 -0400

    Escape colons in query.

--- Brownian/view/utils/es.py
+++ Brownian/view/utils/es.py
@@ -93,6 +93,7 @@ def queryEscape(query):
         ("\"", "\\\""),
         ("[", "\u005b"),
         ("]", "\u005d"),
+        (":", "\u003a"),
     ]
     for char, replacement in bad_chars:
         query = query.replace(char, replacement)
