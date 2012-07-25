c8f2279d6cdb4fcda7c08c943f515bdc24271fbb
Vlad Grigorescu <grigorescu@gmail.com>
2012-07-25 12:19:42 -0400

    Test update - attempt 2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "Brownian",
-    version = "0.4-r62",
+    version = "0.4-r63",
     packages = ["Brownian", "Brownian.view", "Brownian.view.templatetags", "Brownian.view.utils"],
     install_requires = ["Django", "django-dajax", "django-dajaxice", "requests", "nose", "pytz"],
     package_data = {'Brownian.view':
