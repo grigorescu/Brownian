92be4b5efeaf615bceda5397997ef1e9b57aa8a7
Vlad Grigorescu <grigorescu@gmail.com>
2012-07-25 13:09:19 -0400

    Make tests non-interactive.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "Brownian",
-    version = "0.4-r65",
+    version = "0.4-r67",
     packages = ["Brownian", "Brownian.view", "Brownian.view.templatetags", "Brownian.view.utils"],
     install_requires = ["Django", "django-dajax", "django-dajaxice", "requests", "nose", "pytz"],
     package_data = {'Brownian.view':
