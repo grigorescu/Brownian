275e49e3ddefe70d1a702a574c44c4b39f26e5be
Vlad Grigorescu <grigorescu@gmail.com>
2012-08-09 23:24:41 -0400

    Shard failure is not being correctly reported by ES. Needs more work.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = "Brownian",
-    version = "0.4-r79",
+    version = "0.4-r81",
     packages = ["Brownian", "Brownian.view", "Brownian.view.templatetags", "Brownian.view.utils"],
     install_requires = ["Django", "django-dajax", "django-dajaxice", "requests", "nose", "pytz"],
     package_data = {'Brownian.view':
