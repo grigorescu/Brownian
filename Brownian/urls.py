d07749c68e7518ff970699a5f6d9af47009e4fef
Vlad Grigorescu <grigorescu@gmail.com>
2012-08-17 21:34:30 -0400

    Fix a couple JS issues.

--- Brownian/urls.py
+++ Brownian/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url, include
-from dajaxice.core import dajaxice_config
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+dajaxice_autodiscover()
 
 urlpatterns = patterns('',
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
--- Brownian/view/static/dajaxice/dajaxice.core.js
+++ Brownian/view/static/dajaxice/dajaxice.core.js
@@ -39,7 +39,7 @@ var Dajaxice = {
         var send_data = 'argv='+encodeURIComponent(JSON.stringify(argv));
         var oXMLHttpRequest = new XMLHttpRequest;
 
-        oXMLHttpRequest.open(method, applicationPath + '/dajaxice/'+dajaxice_function+'/');
+        oXMLHttpRequest.open(method, applicationPath + 'dajaxice/'+dajaxice_function+'/');
         oXMLHttpRequest.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         oXMLHttpRequest.setRequestHeader("X-CSRFToken", Dajaxice.get_cookie('csrftoken'));
         oXMLHttpRequest.onreadystatechange = function() {
