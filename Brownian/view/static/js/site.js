9fb5f6bb4969b76448dadd879df94df7b173d6c9
Vlad Grigorescu <grigorescu@gmail.com>
2012-08-30 14:50:43 -0400

    Copy/paste pop-up, fix sorting, disable dropdown on click.

--- Brownian/view/static/js/site.js
+++ Brownian/view/static/js/site.js
@@ -1,6 +1,8 @@
 /** Local JS functions **/
 
 function appendToQuery(filter, negated, query, operator){
+    $('.dropdown.open .dropdown-toggle').dropdown('toggle');
+
     if (operator == undefined){
         operator = "AND"
     }
@@ -30,6 +32,8 @@ function appendToQuery(filter, negated, query, operator){
 }
 
 function replaceQuery(query, filter, negated){
+    $('.dropdown.open .dropdown-toggle').dropdown('toggle');
+
     if ($(query) == undefined){
         return false;
     }
@@ -47,7 +51,36 @@ function replaceQuery(query, filter, negated){
 }
 
 function ipanywhere(ip){
-    return '(bound"' + ip + '" OR dst:"' + ip + '" OR dst_addr:"' + ip + '" OR host:"' + ip + '" OR id.orig_h:"' + ip + '" OR id.resp_h:"' + ip + '" OR request:"' + ip + '" OR src:"' + ip + '" OR src_addr:"' + ip + '" OR x_originating_ip:"' + ip + '")'
+    return '(bound:"' + ip + '" OR dst:"' + ip + '" OR dst_addr:"' + ip + '" OR host:"' + ip + '" OR id.orig_h:"' + ip + '" OR id.resp_h:"' + ip + '" OR request:"' + ip + '" OR src:"' + ip + '" OR src_addr:"' + ip + '" OR x_originating_ip:"' + ip + '")'
+}
+
+function exportRow(rowNumber){
+    var keys = new Array(0);
+    var values = new Array(0);
+    var count = 0;
+
+    $('th > a[rel="popover"]').each(function(index, value) { keys.push($(this).html());} );
+    $('#results > table > tbody > tr:eq(' + rowNumber + ') > td[class="fieldval"] > ul > li > a').each(function(index, value) {
+        var value = $(this).html();
+        if ( $(this).attr("title") )
+            value = $(this).attr("title");
+        values.push(value.replace('<b class="caret"></b>', '').replace(/(^\s*)|(\s*$)/gi,""));
+    } );
+
+    result = '';
+    for (var i = 0; i < keys.length; i++) {
+        if (i < values.length){
+            if (/([0-9]|[A-Z]|[a-z])/.test(values[i])){
+                result += keys[i] + ": " + values[i] + "\n";
+                count += 1;
+            }
+        }
+    }
+
+    result = '<textarea rows=' + count + ' style="white-space:normal; width:90%">' + result;
+
+    result += '</textarea>';
+    return result;
 }
 
 function replaceSort(tab, value, direction){
