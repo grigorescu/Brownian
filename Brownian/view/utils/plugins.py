c734fbbcb8680f704cfcc5b8ee605c4d0557526d
Vlad Grigorescu <grigorescu@gmail.com>
2012-10-18 15:23:07 -0400

    Use the Team Cymru whois server by default, make it easier to use complex commands, and optionally insert a new line before the output.

--- Brownian/view/utils/plugins.py
+++ Brownian/view/utils/plugins.py
@@ -1,22 +1,26 @@
 import subprocess
 import string
+import shlex
 
 class Plugin:
-    def __init__(self, command, allowedChars):
+    def __init__(self, command, allowedChars, insertInitialNewline=False):
         # We replace the characters we do allow with empty strings, to get a string of all the characters we don't allow.
-        self.notAllowedCharMap = string.maketrans(allowedChars, " "*len(allowedChars))
-        self.command = command
+        self.notAllowedCharMap = str(string.maketrans(allowedChars, " "*len(allowedChars)))
+        self.command = shlex.split(command)
+        self.insertInitialNewline = insertInitialNewline
 
     def run(self, values):
         sanitizedValues = []
         for value in values:
             sanitizedValues.append(str(value).translate(None, self.notAllowedCharMap))
-        result = subprocess.Popen([self.command] + sanitizedValues, stdout=subprocess.PIPE)
+        result = subprocess.Popen(self.command + sanitizedValues, stdout=subprocess.PIPE)
         stdout, stderr = result.communicate()
+        if self.insertInitialNewline:
+            stdout = "\n" + stdout
         return stdout.replace("\n", "<br>")
 
 whois = {"displayName": "Whois Lookup",
-         "plugin": Plugin("whois", string.letters + string.digits + ".:-_")}
+         "plugin": Plugin("whois -h whois.cymru.com \" -p -u\"", string.letters + string.digits + ".:-_", insertInitialNewline=True)}
 
 dns_lookup = {"displayName": "DNS Lookup",
               "plugin": Plugin("host", string.letters + string.digits + ".:-_")}
